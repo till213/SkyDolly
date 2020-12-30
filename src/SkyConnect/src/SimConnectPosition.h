8b9beb87285412acae9dea242b5a2beb1e6a7204
Oliver Knoll <starsky.hutch@gmx.ch>
2020-12-30 16:11:32 +0100

    Simplify CMakeLists.txt.
    - Refactor SimConnectPosition

--- src/SkyConnect/src/SimConnectPosition.h
+++ src/SkyConnect/src/SimConnectPosition.h
@@ -1,6 +1,8 @@
 #ifndef SIMCONNECTPOSITION_H
 #define SIMCONNECTPOSITION_H
 
+#include "../../Kernel/src/Position.h"
+
 struct SimConnectPosition
 {
     double latitude;  // degrees
@@ -10,6 +12,28 @@ struct SimConnectPosition
     double pitch;     // degrees
     double bank;      // degrees
     double heading;   // degrees
+
+    inline Position toPosition() const {
+        Position position;
+
+        position.latitude = latitude;
+        position.longitude = longitude;
+        position.altitude = altitude;
+        position.pitch = pitch;
+        position.bank = bank;
+        position.heading = heading;
+
+        return position;
+    }
+
+    inline void fromPosition(const Position &position) {
+        latitude = position.latitude;
+        longitude = position.longitude;
+        altitude = position.altitude;
+        pitch = position.pitch;
+        bank = position.bank;
+        heading = position.heading;
+    }
 };
 
 #endif // SIMCONNECTPOSITION_H
