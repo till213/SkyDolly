2285af3b898971b8338fbb9a86e8fa7eeb2abf46
Oliver Knoll <starsky.hutch@gmx.ch>
2021-02-27 15:34:53 +0100

    Create simulation variables dialog.

--- src/UserInterface/src/AboutDialog.h
+++ src/UserInterface/src/AboutDialog.h
@@ -13,7 +13,7 @@ class AboutDialog : public QDialog
 
 public:
     explicit AboutDialog(QWidget *parent = nullptr);
-    ~AboutDialog();
+    virtual ~AboutDialog();
 
 private:
     Ui::AboutDialog *ui;
