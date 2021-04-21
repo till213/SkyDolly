79d1005a631a13e26b6ac36407121f59569e7ad4
Oliver Knoll <starsky.hutch@gmx.ch>
2021-04-21 08:49:04 +0200

    FIX: Properly name Yoke X Position simulation variable (not Y)
    (cherry picked from commit 244eb3ae591cca05f5d1e39cada961aa7fae8f67)

--- src/Model/src/SimVar.cpp
+++ src/Model/src/SimVar.cpp
@@ -38,7 +38,7 @@ const char *SimVar::RotationVelocityBodyX = "Rotation Velocity Body X";
 const char *SimVar::RotationVelocityBodyY = "Rotation Velocity Body Y";
 const char *SimVar::RotationVelocityBodyZ = "Rotation Velocity Body Z";
 
-const char *SimVar::YokeXPosition = "Yoke Y Position";
+const char *SimVar::YokeXPosition = "Yoke X Position";
 const char *SimVar::YokeYPosition = "Yoke Y Position";
 const char *SimVar::RudderPosition = "Rudder Position";
 const char *SimVar::ElevatorPosition = "Elevator Position";
