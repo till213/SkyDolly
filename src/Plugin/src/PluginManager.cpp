dfc038aa91c027a1b18fccfcd7230a687e31ce93
Oliver Knoll <starsky.hutch@gmx.ch>
2021-07-06 08:30:59 +0200

    Add initial PluginManager

---
+++ src/Plugin/src/PluginManager.cpp
@@ -0,0 +1,71 @@
+/**
+ * Sky Dolly - The black sheep for your flight recordings
+ *
+ * Copyright (c) Oliver Knoll
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+#include <memory>
+
+#include "PluginManager.h"
+
+class PluginManagerPrivate
+{
+public:
+    PluginManagerPrivate() noexcept
+    {}
+
+    ~PluginManagerPrivate() noexcept
+    {}
+
+    static PluginManager *instance;
+};
+
+PluginManager *PluginManagerPrivate::instance = nullptr;
+
+// PUBLIC
+
+PluginManager &PluginManager::getInstance() noexcept
+{
+    if (PluginManagerPrivate::instance == nullptr) {
+        PluginManagerPrivate::instance = new PluginManager();
+    }
+    return *PluginManagerPrivate::instance;
+}
+
+void PluginManager::destroyInstance() noexcept
+{
+    if (PluginManagerPrivate::instance != nullptr) {
+        delete PluginManagerPrivate::instance;
+        PluginManagerPrivate::instance = nullptr;
+    }
+}
+
+
+// PROTECTED
+
+PluginManager::~PluginManager() noexcept
+{}
+
+// PRIVATE
+
+PluginManager::PluginManager() noexcept
+    : d(std::make_unique<PluginManagerPrivate>())
+{}
