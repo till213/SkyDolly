af21dc66c25cec3810d8d715105839a7f3f9e14b
Oliver Knoll <starsky.hutch@gmx.ch>
2021-07-23 14:41:20 +0200

    Introduce "active button"
    - Displays the "active" pixmap while the button is pressed

---
+++ src/Widget/src/ActiveButton.h
@@ -0,0 +1,61 @@
+/**
+ * Sky Dolly - The black sheep for your flight recordings
+ *
+ * Copyright (c) Oliver Knoll
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+#ifndef ACTIVEBUTTON_H
+#define ACTIVEBUTTON_H
+
+#include <memory>
+
+#include <QPushButton>
+
+class QMouseEvent;
+
+#include "WidgetLib.h"
+
+/*!
+ * Displays the \em active icon when the button is pressed.
+ *
+ * Implementation note: the default QPushButton does not seem
+ * to honour the "active" pixmap in the given QIcon. Also refer
+ * to:
+ * https://stackoverflow.com/questions/40318759/change-qpushbutton-icon-on-hover-and-pressed
+ */
+class ActiveButtonPrivate;
+
+class WIDGET_API ActiveButton : public QPushButton
+{
+    Q_OBJECT
+public:
+    ActiveButton(QWidget *parent) noexcept;
+    virtual ~ActiveButton() noexcept;
+
+protected:
+    virtual void mousePressEvent(QMouseEvent *e) noexcept override;
+    virtual void mouseReleaseEvent(QMouseEvent *e) noexcept override;
+
+private:
+    std::unique_ptr<ActiveButtonPrivate> d;
+};
+
+#endif // ACTIVEBUTTON_H
