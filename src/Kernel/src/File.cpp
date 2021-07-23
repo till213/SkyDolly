3da8c852e1017cff6aeddbb4197003005eadefc7
Oliver Knoll <starsky.hutch@gmx.ch>
2021-07-23 16:50:58 +0200

    Auto-suggest export file paths
    - Option to open exported file after export

--- src/Kernel/src/File.cpp
+++ src/Kernel/src/File.cpp
@@ -22,57 +22,20 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-#ifndef KMLEXPORTDIALOG_H
-#define KMLEXPORTDIALOG_H
+#include <QString>
+#include <QStringBuilder>
 
-#include <QDialog>
+#include "File.h"
 
-namespace Ui {
-    class KMLExportDialog;
-}
+// PUBLIC
 
-class KMLExportDialogPrivate;
-
-class KMLExportDialog : public QDialog
+QString File::ensureSuffix(const QString filePath, const QString &suffix) noexcept
 {
-    Q_OBJECT
-public:
-    /*!
-     * Resampling period [millisecons]
-     */
-    enum class ResamplingPeriod {
-        Original = 0,
-        TenHz = 100,
-        FiveHz = 200,
-        TwoHz = 500,
-        OneHz = 1000,
-        AFifthHz = 5000,
-        ATenthHz = 10000
-    };
-    static constexpr char FileSuffix[] = "kml";
-
-    explicit KMLExportDialog(QWidget *parent = nullptr) noexcept;
-    virtual ~KMLExportDialog() noexcept;
-
-    QString getSelectedFilePath() const noexcept;
-    ResamplingPeriod getSelectedResamplingPeriod() const noexcept;
-    bool doOpenExportedFile() const noexcept;
-
-private:
-    Ui::KMLExportDialog *ui;
-    std::unique_ptr<KMLExportDialogPrivate> d;
-
-    void initUi() noexcept;
-    void updateInfoUi() noexcept;
-    void frenchConnection() noexcept;
-
-    qint64 estimateNofSamplePoints() noexcept;
-
-private slots:
-    void updateUi() noexcept;
-
-    void on_fileSelectionPushButton_clicked() noexcept;
-    void on_resamplingComboBox_activated(int index) noexcept;
-};
-
-#endif // KMLEXPORTDIALOG_H
+    QString filePathWithSuffix;
+    if (!filePath.endsWith(suffix, Qt::CaseInsensitive)) {
+        filePathWithSuffix = filePath % "." % suffix;
+    } else {
+        filePathWithSuffix = filePath;
+    }
+    return filePathWithSuffix;
+}
