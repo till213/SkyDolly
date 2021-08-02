7bbfeae15583b41ee09beaaae00ee46f4493ea84
Oliver Knoll <starsky.hutch@gmx.ch>
2021-08-02 20:27:14 +0200

    Basic position parsing

--- src/Model/src/Position.cpp
+++ src/Model/src/Position.cpp
@@ -108,7 +108,7 @@ const PositionData &Position::interpolate(qint64 timestamp, TimeVariableData::Ac
         double tn;
         switch (access) {
         case TimeVariableData::Access::Linear:
-            if (SkySearch::getCubicInterpolationSupportData(d->positionData, adjustedTimestamp, SkySearch::DefaultInterpolationWindow, d->currentIndex, &p0, &p1, &p2, &p3)) {
+            if (SkySearch::getCubicInterpolationSupportData(d->positionData, adjustedTimestamp, SkySearch::PositionInterpolationWindow, d->currentIndex, &p0, &p1, &p2, &p3)) {
                 tn = SkySearch::normaliseTimestamp(*p1, *p2, adjustedTimestamp);
             }
             break;
