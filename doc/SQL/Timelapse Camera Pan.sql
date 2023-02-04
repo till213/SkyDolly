11b9fb0373e2a9085024bb6b4aa36eeb392110fc
Oliver Knoll <starsky.hutch@gmx.ch>
2023-02-04 16:42:57 +0100

    Update SQL examples

--- doc/SQL/Timelapse Camera Pan.sql
+++ doc/SQL/Timelapse Camera Pan.sql
@@ -1,12 +1,10 @@
 insert into flight (title, user_aircraft_seq_nr)
-values ('Camera pan', 1);
+values ('LSZH camera pan', 1);
 
 select max(id) from flight;
 
-insert into aircraft (flight_id, seq_nr, type)
-values ((select max(id) from flight), 1, 'Bananas');
-
-select * from aircraft_type;
+select * from aircraft_type
+order by type;
 
 insert into aircraft (flight_id, seq_nr, type)
 values ((select max(id) from flight), 1, 'Asobo XCub');
@@ -14,8 +12,12 @@ values ((select max(id) from flight), 1, 'Asobo XCub');
 select * from aircraft
 order by id desc;
 
+-- Pan from 47.450681, 8.549628 at 1417.0 + 150 feet altitude (about 50 metres above ground)...
 insert into position (aircraft_id, timestamp, latitude, longitude, altitude, pitch, bank, true_heading)
-values ((select max(id) from aircraft), 0, 47, 31, 15000, 0, 0, 90);
+values ((select max(id) from aircraft), 0, 47.450681, 8.549628, 1417.0 + 150, 0, 0, 340);
 
+-- ...  to 47.454667, 8.571006, within 32 * 60 * 1000 [ms] = 32 minutes
 insert into position (aircraft_id, timestamp, latitude, longitude, altitude, pitch, bank, true_heading)
-values ((select max(id) from aircraft), 30 * 1000, 48, 31, 15000, 0, 0, 90);
+values ((select max(id) from aircraft), 32 * 60 * 1000, 47.454667, 8.571006, 1417.0 + 150, 0, 0, 15);
+
+commit;
