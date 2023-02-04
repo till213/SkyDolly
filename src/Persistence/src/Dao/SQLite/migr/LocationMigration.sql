d0bca2755a4c5731c4fa69e7668f4d9e15e43ee3
Oliver Knoll <starsky.hutch@gmx.ch>
2023-02-04 21:02:04 +0100

    FIX: Version bump migration steps must have unique UUIDs
    - Yeah, "unique unique" ;)
    - Across logbook and location migrations, that is
    
    (cherry picked from commit 09c82b7c95c6075770a46c92a753b716f20a2c2c)

--- src/Persistence/src/Dao/SQLite/migr/LocationMigration.sql
+++ src/Persistence/src/Dao/SQLite/migr/LocationMigration.sql
@@ -409,6 +409,6 @@ values
 update metadata
 set app_version = '0.14.0';
 
-@migr(id = "fbfeec8f-0e50-4efc-956d-391e40563b28", descn = "Update application version to 0.15", step = 1)
+@migr(id = "6a3f5879-8974-4685-9285-ec1f174fdacb", descn = "Update application version to 0.15", step = 1)
 update metadata
 set app_version = '0.15.0';
