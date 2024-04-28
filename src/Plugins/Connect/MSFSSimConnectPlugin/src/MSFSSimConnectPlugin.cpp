2640199dcd7557e03f57f3e874ea67ccd65311cf
Oliver Knoll <starsky.hutch@gmx.ch>
2024-04-28 23:31:23 +0200

    Do not ignore local SimConnect.cfg
    - TODO:
      * Add option (settings) to select configuration index
      * Deply SimConnect.cfg with Sky Dolly

--- src/Plugins/Connect/MSFSSimConnectPlugin/src/MSFSSimConnectPlugin.cpp
+++ src/Plugins/Connect/MSFSSimConnectPlugin/src/MSFSSimConnectPlugin.cpp
@@ -529,7 +529,7 @@ bool MSFSSimConnectPlugin::connectWithSim() noexcept
 {
     const auto hWnd = reinterpret_cast<HWND>(d->eventWidget->winId());
     const DWORD userEvent = EventWidget::SimConnectUserMessage;
-    const HRESULT result = ::SimConnect_Open(&(d->simConnectHandle), ::ConnectionName, hWnd, userEvent, nullptr, ::SIMCONNECT_OPEN_CONFIGINDEX_LOCAL);
+    const HRESULT result = ::SimConnect_Open(&(d->simConnectHandle), ::ConnectionName, hWnd, userEvent, nullptr, 0);
     if (result == S_OK) {
         d->eventStateHandler->setSimConnectHandle(d->simConnectHandle);
         d->simConnectAi = std::make_unique<SimConnectAi>(d->simConnectHandle);
