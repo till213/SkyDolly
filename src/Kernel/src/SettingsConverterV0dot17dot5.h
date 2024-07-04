18a550c0e7327516fed25990225e3419fa8a0942
Oliver Knoll <starsky.hutch@gmx.ch>
2024-07-04 13:07:24 +0200

    FIX: Correct the "off by one" MSFSSimConnect connection type (#159)
    - "enum struct ConnectionType" values were "off by one"
    - Correct existing plugin settings
    - Regression introduced with v0.17.1

--- src/Kernel/src/SettingsConverterV0dot17dot5.h
+++ src/Kernel/src/SettingsConverterV0dot17dot5.h
@@ -22,32 +22,54 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-#ifndef SETTINGSCONVERTERV0DOT18_H
-#define SETTINGSCONVERTERV0DOT18_H
+#ifndef SETTINGSCONVERTERV0DOT17DOT5_H
+#define SETTINGSCONVERTERV0DOT17DOT5_H
 
+#include <QString>
+#include <QStringLiteral>
+#include <QStringBuilder>
 #include <QSettings>
+#include <QByteArray>
+#include <QUuid>
 
 #include <Const.h>
 #include <Version.h>
 
-#include "SettingsConverterV0dot17dot5.h"
+#include "SettingsConverterV0dot17.h"
 
-class SettingsConverterV0dot18
+class SettingsConverterV0dot17dot5
 {
 public:
     static inline void convert(const Version &settingsVersion, QSettings &settings) noexcept
     {
-        if (settingsVersion < Version("0.17.5")) {
-            SettingsConverterV0dot17dot5::convert(settingsVersion, settings);
+        if (settingsVersion < Version(QStringLiteral("0.17.0"))) {
+            SettingsConverterV0dot17::convert(settingsVersion, settings);
         }
-        settings.remove("Recording");
-        convertPlugins(settings);
+        // The regression ("off by one" connection type) has been introduced with v0.17.1
+        if (settingsVersion >= Version(QStringLiteral("0.17.1"))) {
+            convertMsfsSimConnectPlugin(settings);
+        }
     }
 
 private:
-    static inline void convertPlugins(QSettings &settings) noexcept
+    static inline void convertMsfsSimConnectPlugin(QSettings &settings) noexcept
     {
+        static const QString ConnectionTypeKey {"ConnectionType"};
+        int connectionType {0};
+
+        // MSFS SimConnect
+        settings.beginGroup(QStringLiteral("Plugins/") % QUuid(Const::MsfsSimConnectPluginUuid).toByteArray());
+        {
+            bool ok {true};
+            connectionType = settings.value(ConnectionTypeKey).toInt(&ok);
+            if (ok) {
+                // Fix existing "off by one" value
+                const int newConnectionType = connectionType > 0 ? connectionType - 1 : 0;
+                settings.setValue(ConnectionTypeKey, newConnectionType);
+            }
+        }
+        settings.endGroup();
     }
 };
 
-#endif // SETTINGSCONVERTERV0DOT18_H
+#endif // SETTINGSCONVERTERV0DOT17DOT5_H
