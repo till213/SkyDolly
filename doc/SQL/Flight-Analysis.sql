110897f22e4b282b02beb7c74088499ee2b0bd99
Oliver Knoll <starsky.hutch@gmx.ch>
2024-08-10 09:31:15 +0200

    Reformat SQL, add terminating semicolon after each SQL

--- doc/SQL/Flight-Analysis.sql
+++ doc/SQL/Flight-Analysis.sql
@@ -1,20 +1,20 @@
 -- Get specific flight given by its ID
 select f.id, f.creation_time, f.title
 from flight f
-where f.id = 42
+where f.id = 42;
 
 -- Get latest flight on a given date
 select f.id, f.creation_time, f.title
 from flight f
 where strftime('%Y-%m-%d', f.creation_time) = '2021-05-26'
 order by f.creation_time desc
-limit 1
+limit 1;
 
 -- Get most recent flight of the logbook
 select f.id, f.creation_time, f.title
 from flight f
 order by f.id desc
-limit 1
+limit 1;
 
 -- Select specific values from tables position, attitude, aircraft and flight
 -- https://zzzcode.ai/answer-question?id=307fbcc3-77bb-4553-91d0-461edb1da0e6
@@ -47,23 +47,23 @@ with ranked_by_timestamp as (
     where  f.id = (select ff.id
                    from   flight ff
                    order by ff.id desc
-                   limit 1)
+                   limit 1);
 )
- select r.flight_id,
-        r.start_zulu_sim_time,
-        r.end_zulu_sim_time,
-        r.flight_number,
-        r.type,
-        r.pos_timestamp,
-        r.latitude,
-        r.longitude,
-        r.altitude,
-        r.att_timestamp,
-        r.pitch,
-        r.bank,
-        r.true_heading,
-        r.speed_in_knots,
-        r.on_ground
+select r.flight_id,
+       r.start_zulu_sim_time,
+       r.end_zulu_sim_time,
+       r.flight_number,
+       r.type,
+       r.pos_timestamp,
+       r.latitude,
+       r.longitude,
+       r.altitude,
+       r.att_timestamp,
+       r.pitch,
+       r.bank,
+       r.true_heading,
+       r.speed_in_knots,
+       r.on_ground
 from  ranked_by_timestamp r
 where r.rn = 1;
 
@@ -86,9 +86,9 @@ on     a.flight_id = f.id
 and    a.seq_nr = 1
 -- Select the above data from the most recent flight in the logbook
 where  f.id = (select ff.id
-        from flight ff
-        order by ff.id desc
-        limit 1)
+               from   flight ff
+               order by ff.id desc
+               limit 1);
 
 -- Select the top 10 aircraft types (number of flights)
 select a.type        as AircraftType,
@@ -96,7 +96,7 @@ select a.type        as AircraftType,
 from   aircraft a
 group by a.type
 order by count(a.type) desc
-limit 10
+limit 10;
 
 -- Select the top 10 flights (flight duration in minutes)
 select f.id, f.title, a.type, max(p.timestamp) / 1000 / 60 as minutes
@@ -107,7 +107,7 @@ join   flight f
 on     a.flight_id = f.id
 group by p.aircraft_id
 order by minutes desc
-limit 10
+limit 10;
 
 -- Select the top 10 aircraft types (total flight time)
 select a.type, sum(a.minutes) as total_minutes
@@ -118,4 +118,4 @@ from (select aa.type, max(pp.timestamp) / 1000 / 60 as minutes
       group by pp.aircraft_id) a
 group by a.type
 order by total_minutes desc
-limit 10
+limit 10;
