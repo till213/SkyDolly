5b2df635a379a04dc660de1a4c55a4ba6f916086
Oliver Knoll <starsky.hutch@gmx.ch>
2024-10-09 08:17:42 +0200

    Replace deprecated method calls

--- src/Plugins/Module/Formation/src/FormationWidget.cpp
+++ src/Plugins/Module/Formation/src/FormationWidget.cpp
@@ -342,7 +342,7 @@ void FormationWidget::frenchConnection() noexcept
             this, &FormationWidget::onVerticalDistanceChanged);
     connect(d->positionButtonGroup, &QButtonGroup::idClicked,
             this, &FormationWidget::onRelativePositionChanged);
-    connect(ui->relativePositionCheckBox, &QCheckBox::stateChanged,
+    connect(ui->relativePositionCheckBox, &QCheckBox::checkStateChanged,
             this, &FormationWidget::onInitialPositionPlacementChanged);
     connect(ui->replayModeComboBox, &QComboBox::activated,
             this, &FormationWidget::onReplayModeSelected);
@@ -1030,8 +1030,9 @@ void FormationWidget::onSelectionChanged() noexcept
     updateToolTips();
 }
 
-void FormationWidget::onInitialPositionPlacementChanged(bool enable) noexcept
+void FormationWidget::onInitialPositionPlacementChanged(Qt::CheckState state) noexcept
 {
+    const auto enable = state == Qt::CheckState::Checked;
     d->moduleSettings.setRelativePositionPlacementEnabled(enable);
 }
 
